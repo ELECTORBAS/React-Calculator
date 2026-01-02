92fdf93c37c416faaec86d0b23ec88675e1a63b3
Anas <xhinling288@gmail.com>
2026-01-03 02:15:53 +0500

    Ready

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,33 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [display, setDisplay] = useState('');
+
+  function clearDisplay() {
+    setDisplay('');
+  }
+
+  function appendToDisplay(value) {
+    setDisplay(prev => prev + value);
+  }
+
+  function equals(){
+    try {
+      setDisplay(eval(display).toString());
+    } catch (error) {
+      setDisplay(error)
+    }
+  }
 
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>Calculator App</h1>
+        <div className="display">{display}</div>
+        <div className="btns">
+          <button onClick={() => appendToDisplay("7")}>7</button>
+          <button onClick={() => appendToDisplay("8")}>8</button>
+          <button onClick={() => appendToDisplay("9")}>9</button>
+          <button className='operators' onClick={clearDisplay}>AC</button>
+          
+
+          <button onClick={() => appendToDisplay("4")}>4</button>
+          <button onClick={() => appendToDisplay("5")}>5</button>
+          <button onClick={() => appendToDisplay("6")}>6</button>
+          <button className='operators' onClick={() => appendToDisplay('-')}>-</button>
+
+          <button onClick={() => appendToDisplay("1")}>1</button>
+          <button onClick={() => appendToDisplay("2")}>2</button>
+          <button onClick={() => appendToDisplay("3")}>3</button>
+          <button className='operators' onClick={() => appendToDisplay('+')}>+</button>
+
+          <button className='operators' onClick={() => appendToDisplay('/')}>/</button>
+          <button onClick={() => appendToDisplay("0")}>0</button>
+          <button className='operators' onClick={() => appendToDisplay('*')}>*</button>
+          
+          <button className='operators' onClick={equals}>=</button>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
